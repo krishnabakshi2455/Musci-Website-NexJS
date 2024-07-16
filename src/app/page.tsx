f1889f1f979ce4b7bf09c05ba6b96020768f8651
krishnabakshi2455 <krishnabakshi2455@gmail.com>
2024-07-16 22:15:10 +0530

    navbar + hero-section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
+import { HeroSection } from "@/components/HeroSection";
 
 
 
 export default function Home() {
   return (
    <>
-   <main className=" min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-    <h1>Hello from page main </h1>
+   <main className=" min-h-screen bg-black/[8.96] antialiased bg-grid-white/[0.02]">
+    <HeroSection/>
    </main>
    
    </>
